7c869827efa71930f3d0d17f77b276b91c75da02
waeletteyeb <wael.etteyeb@etudiant-enit.utm.tn>
2024-10-27 13:03:32 +0100

    tp4

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CartComponent } from './cart/cart.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CatalogComponent,
+    SiteHeaderComponent,
+    ProductDetailsComponent,
+    CartComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule, AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
